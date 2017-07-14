b6fec778f77ad67fe46df507f0f7ebbd6c4d4f45
lydiaxnguyen <lydiaxnguyen@gmail.com>
2017-07-14 13:13:37 -0400

    added all files

---
+++ lydiatnguyen-convert-docs.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+# Declare input argument as a variable
+INPUTFILE=polen.md
+OUTPUTFILE=$INPUTFILE
+# Convert markdown to HTML
+pandoc polen.md -o polen.html
+echo "Converted Polen Markdown to HTML"
+# Convert markdown to DOCX
+pandoc polen.md -o polen.docx
+echo "Converted Polen Markdown to Docx"
+# Convert markdown to ODT
+pandoc polen.md -o polen.odt
+echo "Converted Polen Markdown to ODT"
+# Convert markdown to PDF
+pandoc polen.md --latex-engine=pdflatex -o polen.pdf
+echo "Converted Polen Markdown to PDF"
+# Make the command line read "Converted INPUT-FILENAME to HTML, DOCX, ODT, PDF
